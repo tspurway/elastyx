15a7c386e8a9df00ee1c518dca9635acba9a99ed
Daniel Thornton <dthornton@mozilla.com>
2014-12-10 18:15:07 +0000

    separate bucket from commands, and remove s3 uris from mrjob.conf

--- persistent_cluster_example.sh
+++ persistent_cluster_example.sh
@@ -2,15 +2,17 @@ job_id=$(python2 -m mrjob.tools.emr.create_job_flow -c mrjob.conf)
 
 step_num=1
 
+bucket=tiles-stage-1-queue-s3-bucket-abcdefghijkl
+
 python2 elastyx.py \
     -c mrjob.conf \
     -r emr \
     --no-bootstrap-mrjob \
     --hadoop-arg -libjars \
     --hadoop-arg /home/hadoop/elastyxOutput.jar,/home/hadoop/org.json.jar \
+    --s3-log-uri s3://$bucket/log/ \
+    --s3-scratch-uri s3://$bucket/tmp/ \
     --no-output \
-    --output-dir s3://tiles-stage-1-queue-s3-bucket-abcdef/elastyx/$step_num/ \
-    s3://tiles-stage-1-queue-s3-bucket-abcdefg/impression/2099-12-31/zyxwvut \
+    --output-dir s3://$bucket/elastyx/$step_num/ \
+    s3://$bucket/impression/ \
     --emr-job-flow-id $job_id
-
-
--- simple_emr_example.sh
+++ simple_emr_example.sh
@@ -1,11 +1,15 @@
 step_num=1
 
+bucket=tiles-stage-1-queue-s3-bucket-abcdefghijkl
+
 python2 elastyx.py \
     -c mrjob.conf \
     -r emr \
     --no-bootstrap-mrjob \
     --hadoop-arg -libjars \
     --hadoop-arg /home/hadoop/elastyxOutput.jar,/home/hadoop/org.json.jar \
+    --s3-log-uri s3://$bucket/log/ \
+    --s3-scratch-uri s3://$bucket/tmp/ \
     --no-output \
-    --output-dir s3://tiles-stage-1-queue-s3-bucket-abcdef/elastyx/$step_num/ \
-    s3://tiles-stage-1-queue-s3-bucket-abcdefg/impression/2099-12-31/zyxwvut
+    --output-dir s3://$bucket/elastyx/$step_num/ \
+    s3://$bucket/impression/
